7dfdd75f96569043215a94dfae1abf26c65a406b
zhangqing.mmqg <zhangqing.mmqg@bytedance.com>
2026-08-28 14:50:53 +0800

    修改传递

--- pkg/ctxutil/ctx_meta.go
+++ pkg/ctxutil/ctx_meta.go
@@ -47,8 +47,7 @@ func SetMeta(ctx context.Context, traceId string, userId int64) context.Context 
 	if ctx == nil {
 		return ctx
 	}
-	return metainfo.WithPersistentValues(ctx,
-		TraceID, traceId,
-		UserID, strconv.FormatInt(userId, 10),
-	)
+	ctx = metainfo.WithPersistentValue(ctx, TraceID, traceId)
+	ctx = metainfo.WithPersistentValue(ctx, UserID, strconv.FormatInt(userId, 10))
+	return ctx
 }
